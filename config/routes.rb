0aa8f5abc380f774bdf38cae6494a55e5a04e64f
ebere10 <ebere10@hotmail.com>
2017-02-20 15:43:25 +0000

    Clientside authentication working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :users, only: [:index, :show]
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
 end
