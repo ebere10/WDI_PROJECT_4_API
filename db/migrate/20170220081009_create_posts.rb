97fd2035d0da80450c5916fa83611cfe4d0ead9f
ebere10 <ebere10@hotmail.com>
2017-02-22 09:45:46 +0000

    added data to seeds file

--- db/migrate/20170220081009_create_posts.rb
+++ db/migrate/20170220081009_create_posts.rb
@@ -8,18 +8,3 @@ class CreatePosts < ActiveRecord::Migration[5.0]
     end
   end
 end
-
-
-create table i sent
-db  create migrate
-
-then
-model sort out relationship (sent you)
-
-then
-controllers make new file messages
-inside have index.js
-and show.js (maybe)
-
-views new folder comments
-index.html and show (maybe)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+u1 = User.create!(email: 'ebere@ebere.com', password: 'password', password_confirmation: 'password')
+
+u2 = User.create!(email: 'rainey@rainey.com', password: 'password', password_confirmation: 'password')
+
+u3 = User.create!(email: 'bob@bob.com', password: 'password', password_confirmation: 'password')
+
+p1 = u1.posts.create!(body: 'Why do we have random thoughts?')
+
+p2 = u2.posts.create!(body: 'I\'ve never seen my face with my own eyes, I\'ve only seen it in a mirror!')
+
+p3 = u3.posts.create!(body: 'If the universe is infinite, how can it be expanding?')
+
+p4 = u1.posts.create!(body: 'How many times can you say “What?” before you just nod and smile because you still didn’t hear what someone said?')
+
+p5 = u2.posts.create!(body: 'Sometimes I’ll look down at my watch 3 consecutive times and still not know what time it is')
+
+Comment.create!(user: u2, post: p1, body: 'That is a good question!')
+
+Comment.create!(user: u1, post: p4, body: 'Lol, I usually say it twice before giving up!')
